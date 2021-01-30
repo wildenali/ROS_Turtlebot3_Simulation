e9128e1a52e80c0eb7b6be3145d42ddc453d7a3b
wildenali <wildenwildenaliali@gmail.com>
2021-01-30 11:20:10 +0700

    Laser Scanner - how to convert laser scan to pointcloud

---
+++ turtlebot3_wilden/src/A_LaserScanner/A03_ConvertLaserScanIntoAPointcloud.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import rospy
+from sensor_msgs.msg import PointCloud2 as pc2
+from sensor_msgs.msg import LaserScan
+from laser_geometry import LaserProjection
+
+
+class LaserToPointCloud():
+    def __init__(self):
+        self.laserProj = LaserProjection()
+        self.pcPub = rospy.Publisher("/laser_point_cloud", pc2, queue_size=1)
+        self.laserSub = rospy.Subscriber(
+            "/scan", LaserScan, self.laserCallBack)
+
+    def laserCallBack(self, data):
+        cloud_out = self.laserProj.projectLaser(data)
+        self.pcPub.publish(cloud_out)
+
+
+if __name__ == '__main__':
+    rospy.init_node("laserToPointCloud")
+    laserToPointCloud = LaserToPointCloud()
+    rospy.spin()
