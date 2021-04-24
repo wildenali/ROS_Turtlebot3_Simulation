ef096318418962d98fab4276cfe66dab6e936b28
wildenali <wildenwildenaliali@gmail.com>
2021-04-24 05:23:24 +0000

    D_MazeSolving - How to calculate angle error

---
+++ turtlebot3_wilden/src/D_MazeSolving/D02_TwoWaypoinAndHeading.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+
+import rospy
+from nav_msgs.msg import Odometry
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
+from geometry_msgs.msg import Point, Twist
+from math import atan2, pi, sqrt
+
+pos_x = 0.0
+pos_y = 0.0
+
+roll  = 0.0
+pitch = 0.0
+yaw   = 0.0
+
+kP_Linear = 0.05
+kP_Angular = 0.5
+
+def odomCallback(msg):
+    global pos_x, pos_y, yaw
+
+    pos_x = msg.pose.pose.position.x
+    pos_y = msg.pose.pose.position.y
+
+    orientation_q = msg.pose.pose.orientation
+    orient_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
+    (roll, pitch, yaw) = euler_from_quaternion(orient_list)
+
+def stopTwist():
+    print("Stop the Robot")
+    speed.linear.x  = 0.0
+    speed.angular.z = 0.0
+    pub.publish(speed)
+    
+def run():
+    print("Run")
+    while not rospy.is_shutdown():
+
+        # How to calculate distance between current position with goal position
+        length_x = goal.x - pos_x
+        length_y = goal.y - pos_y
+        angle_to_goal = atan2(length_y, length_x)
+        distance_to_goal = sqrt(length_x * length_x + length_y * length_y)
+
+        # How Calculate angle error
+        # quat = quaternion_from_euler(roll, pitch, yaw)
+        # print(quat)
+        # print(yaw)
+        error_yaw = angle_to_goal - yaw
+        print("angle to goal: {:.3f}  yaw: {:.3f}  distance: {:.3f}  speed linear x: {:.3f}".format(error_yaw, speed.angular.z, distance_to_goal, speed.linear.x))
+        
+        # speed.linear.x = kP_Linear * (distance_to_goal)
+        speed.angular.z = kP_Angular * error_yaw
+        # print("target_rad:{}  current yaw:{}  distance_to_goal:{}".format(target_rad, yaw, distance_to_goal))
+
+        
+        # if error_yaw < 0.1 and error_yaw > -0.1:    # 0.1 on radian unit
+        #     speed.linear.x = kP_Linear * (distance_to_goal)
+        #     # speed.angular.z = kP_Angular * error_yaw
+        #     speed.angular.z = 0.0
+        # else:
+        #     speed.linear.x = 0.0
+        #     speed.angular.z = kP_Angular * error_yaw
+        # print("error yaw: {:.3f}  speed angular z: {:.3f}  distance: {:.3f}  speed linear x: {:.3f}".format(error_yaw, speed.angular.z, distance_to_goal, speed.linear.x))
+        pub.publish(speed)
+        rate.sleep()
+
+rospy.init_node("two_waypoint_and_heading")
+sub = rospy.Subscriber("/odom", Odometry, odomCallback)
+pub = rospy.Publisher("/cmd_vel", Twist, queue_size=1)
+
+speed = Twist()
+rate  = rospy.Rate(5)
+goal  = Point()
+
+# 
+
+'''
+contoh xy = 0 -1
+            y
+-1 1       0 1       1 1
+            |
+            |
+            |
+-1 0--------|--------1 0 x
+            |
+            |
+            |
+            |
+-1 -1      0 -1      1 -1
+'''
+goal.x  = 0         # units in meter  0  0 -1  1  1 -1
+goal.y  = -1         # units in meter -1  1  0  0  1  1
+
+if __name__ == '__main__':
+    try:
+        run()
+    except rospy.ROSInterruptException:
+        pass
--- turtlebot3_wilden/src/D_MazeSolving/README.md
+++ turtlebot3_wilden/src/D_MazeSolving/README.md
@@ -43,3 +43,47 @@ How to move the robot to certain position using odometry for position and euler,
    - `$ catkin_make`
    - `$ source devel/setup.bash`
    - `$ rosrun turtlebot3_wilden D01_ReachCertainPosition.py`
+
+## D02_TwoWaypoinAndHeading
+
+How to move the robot to certain position using odometry for position and euler, atan2 for desired angle
+
+1. Create a file called D02_TwoWaypoinAndHeading.py
+
+   - `$ roscd turtlebot3_wilden/src/D_MazeSolving`
+   - `$ touch D02_TwoWaypoinAndHeading.py`
+   - `$ chmod +x D02_TwoWaypoinAndHeading.py` change the permissions
+
+2. Launch the turtlebot3_empty_world.launch
+   Open a terminal and run:
+
+   - `$ export TURTLEBOT3_MODEL=burger`
+   - `$ roslaunch turtlebot3_gazebo turtlebot3_empty_world.launch`
+
+3. Find the /odom Topic
+   Open a new terminal and run:
+
+   - `$ rostopic list`
+   - `$ rostopic info /odom`
+   - `$ rosmsg show nav_msgs/Odometry`
+   - `$ rostopic echo /odom -n1`
+   - `$ rostopic echo /odom`
+
+4. Move to certain point using python script
+   Open a new terminal and run:
+
+   - Open a D02_TwoWaypoinAndHeading.py file and write the code
+   - Open a CMakeLists.txt inside turtlebot3_wilden package and edit
+
+   ```
+   catkin_install_python(PROGRAMS
+       src/D_MazeSolving/D02_TwoWaypoinAndHeading.py
+       DESTINATION ${CATKIN_PACKAGE_BIN_DESTINATION}
+   )
+   ```
+
+   - Open a D02_TwoWaypoinAndHeading.py file and write the code
+   - `$ cd ~/catkin_ws`
+   - `$ catkin_make`
+   - `$ source devel/setup.bash`
+   - `$ rosrun turtlebot3_wilden D02_TwoWaypoinAndHeading.py`
